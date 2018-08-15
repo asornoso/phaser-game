47306ce5f2dcaacf22a53356a09d2d0c8c6b8adb
Adam Sornoso <adamsornoso@Admins-iMac.local>
2018-08-15 08:06:15 -0700

    score and game state

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-let width = 1400
-let height = 800
+let width = window.innerWidth
+let height = window.innerHeight
 let gravity = 200
 
 let player_height = 75
@@ -12,17 +12,21 @@ let enemy_height = 70
 let enemy_width = 70
 let enemy_speed = 5
 
-//Generate enemy every x milisecond
-let enemy_generator = 80
+//Points added per enemy beat
+let pointsPerEnemy = 5
 
+let enemy_difficulty = 50
 
+let score = 0
+let lives = 3
 
 
 let player
 let enemies = []
 let cursors
-let time = 0
-
+let scoreText
+let livesText
+let state = 'play'
 let config = {
     type: Phaser.AUTO,
     width: width,
@@ -35,9 +39,6 @@ let config = {
     }
 };
 
-function incrementSeconds() {
-  time += 1;
-}
 
 function withinBounds(x, y, direction) {
   if(direction == 'left'){
@@ -72,50 +73,75 @@ function create ()
     player.setOrigin(0,0)
     player.setDisplaySize(player_width, player_height)
 
-    timer = setInterval(incrementSeconds, 1);
+    scoreText = this.add.text(16, 16, 'Score: 0', { fontSize: '32px', fill: '#FFF' });
+    livesText = this.add.text(300, 16, 'Lives: 3', { fontSize: '32px', fill: '#FFF' });
+
 }
 
 function update(){
-  cursors = this.input.keyboard.createCursorKeys();
-  if (cursors.left.isDown && withinBounds(player.x, player.y, 'left')){
-      console.log('left')
-      player.x += player_speed * -1
-  }
-
-  if (cursors.right.isDown && withinBounds(player.x, player.y, 'right')){
-      console.log('right')
-      player.x += player_speed
-  }
-
-  if(cursors.up.isDown && withinBounds(player.x, player.y, 'up')){
-      console.log('up')
-      player.y += player_speed * -1
-  }
-
-  if(cursors.down.isDown && withinBounds(player.x, player.y, 'down')){
-      console.log('down')
-      player.y += player_speed
-  }
-
-  if(time % enemy_generator == 0){
-    let enemy = this.add.sprite(width + enemy_width + 10, 100 + (Math.random() * (height - 200) ), 'enemy')
-    enemy.setOrigin(0,0)
-    enemy.flipX = true
-    enemies.push(enemy)
-    console.log('enemy created')
-  }
-
-  for(let i = 0; i < enemies.length; i++){
-    if(enemies[i].x <= -player_width -10){
-      console.log(enemies[i])
-      enemies[i].destory()
-      enemies.splice(i, 1)
-    }else{
-      enemies[i].x -= enemy_speed
+  if(state == 'play'){
+    cursors = this.input.keyboard.createCursorKeys();
+    if (cursors.left.isDown && withinBounds(player.x, player.y, 'left')){
+        console.log('left')
+        player.x += player_speed * -1
     }
 
+    if (cursors.right.isDown && withinBounds(player.x, player.y, 'right')){
+        console.log('right')
+        player.x += player_speed
+    }
+
+    if(cursors.up.isDown && withinBounds(player.x, player.y, 'up')){
+        console.log('up')
+        player.y += player_speed * -1
+    }
+
+    if(cursors.down.isDown && withinBounds(player.x, player.y, 'down')){
+        console.log('down')
+        player.y += player_speed
+    }
+
+    let random = Math.random() * 1000
+
+    if(random < enemy_difficulty){
+      let enemy = this.add.sprite(width + enemy_width + 10, 40 + (Math.random() * (height - 80) ), 'enemy')
+      enemy.setOrigin(0,0)
+      enemy.setDisplaySize(enemy_width, enemy_height)
+      enemy.flipX = true
+      enemies.push(enemy)
+      console.log('enemy created')
+    }
+
+    for(let i = 0; i < enemies.length; i++){
+
+      if(player.y + player_height > enemies[i].y &&
+        player.x + player_width  > enemies[i].x &&
+        player.y < enemies[i].y + enemy_height &&
+        player.x < enemies[i].x + enemy_width){
+          console.log('HIT')
+          lives--
+          if(lives <= 0){
+            this.add.text(width/2 - 300, height/2 - 100, 'GAME OVER\n Refresh to restart', { fontSize: '64px', fill: '#FFF' });
+            state = 'over'
+          }
+          livesText.setText('Lives: '+ lives)
+          enemies[i].destroy()
+          enemies.splice(i, 1)
+
+          break;
+      }
+
+      if(enemies[i].x <= -player_width -10){
+        enemies[i].destroy()
+
+        enemies.splice(i, 1)
+        score += pointsPerEnemy;
+        scoreText.setText('Score: ' + score);
+
+      }else{
+        enemies[i].x -= enemy_speed
+      }
+
+    }
   }
-  console.log(enemies.length)
-
-
 }
